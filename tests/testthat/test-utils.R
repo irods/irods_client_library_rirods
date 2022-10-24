d7542b532bee956975f695908738d9e12a8c5317
MartinSchobben <martin.schobben@hotmail.com>
2022-10-24 18:18:22 +0200

    include helpers in mocking

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,16 +1,18 @@
-test_that("irods object helpers work", {
+with_mock_dir("helpers", {
+  test_that("irods object helpers work", {
 
-  # path exists
-  expect_true(path_exists("/tempZone/home/bobby"))
-  expect_false(path_exists("/tempZone/home/frank"))
+    # path exists
+    expect_true(path_exists("/tempZone/home/bobby"))
+    expect_false(path_exists("/tempZone/home/frank"))
 
-  # collection exists
-  expect_true(is_collection("/tempZone/home/bobby")) # is a collection
-  expect_false(is_collection("/tempZone/home/bobby/test")) # is a data object
-  expect_error(is_collection("/tempZone/home/frank")) # does not exist at all
+    # collection exists
+    expect_true(is_collection("/tempZone/home/bobby")) # is a collection
+    expect_false(is_collection("/tempZone/home/bobby/test")) # is a data object
+    expect_error(is_collection("/tempZone/home/frank")) # does not exist at all
 
-  # object exists
-  expect_true(is_object("/tempZone/home/bobby/test")) # is a data object
-  expect_false(is_object("/tempZone/home/bobby")) # is a collection
-  expect_error(is_object("/tempZone/home/frank/test")) # does not exist at all
+    # object exists
+    expect_true(is_object("/tempZone/home/bobby/test")) # is a data object
+    expect_false(is_object("/tempZone/home/bobby")) # is a collection
+    expect_error(is_object("/tempZone/home/frank/test")) # does not exist at all
+  })
 })
