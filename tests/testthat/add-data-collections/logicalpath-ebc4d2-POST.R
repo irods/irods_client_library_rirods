0e3a2f68e8db4e7faf98969255f7e2117a9912a9
MartinSchobben <MartinSchobben@users.noreply.github.com>
2022-11-02 12:11:12 +0000

    Snapshots http response

--- tests/testthat/add-data-collections/logicalpath-ebc4d2-POST.R
+++ tests/testthat/add-data-collections/logicalpath-ebc4d2-POST.R
@@ -1,6 +1,6 @@
 structure(list(method = "POST", url = "/logicalpath?logical-path=%2FtempZone%2Fhome%2Fa&collection=1&create-parent-collections=0", 
     status_code = 200L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Wed, 02 Nov 2022 07:37:26 GMT", `Content-Length` = "0", 
+        Date = "Wed, 02 Nov 2022 12:11:04 GMT", `Content-Length` = "0", 
         Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
