5da5fb2d7440bad180da92b0993cba4047383cfd
MartinSchobben <MartinSchobben@users.noreply.github.com>
2022-12-09 12:22:42 +0000

    Snapshots http response

--- tests/testthat/data-objects/logicalpath-371f96-DELETE.R
+++ tests/testthat/data-objects/logicalpath-371f96-DELETE.R
@@ -1,6 +1,6 @@
-structure(list(method = "POST", url = "/logicalpath?logical-path=%2FtempZone%2Fhome%2Frods%2Ftestthat%2Fx%2Fa&collection=1&create-parent-collections=1", 
+structure(list(method = "DELETE", url = "/logicalpath?logical-path=%2FtempZone%2Fhome%2Frods%2Ftestthat%2FBOD.rds&no-trash=1&recursive=0", 
     status_code = 200L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Fri, 09 Dec 2022 12:22:28 GMT", `Content-Length` = "0", 
+        Date = "Fri, 09 Dec 2022 12:22:34 GMT", `Content-Length` = "0", 
         Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
