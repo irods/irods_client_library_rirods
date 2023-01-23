b68dcc6610969e4a108b5aeb02e19b9887c60164
MartinSchobben <MartinSchobben@users.noreply.github.com>
2023-01-23 15:39:01 +0000

    Snapshots http response

--- tests/testthat/metadata-remove/metadata-1956b8-POST.R
+++ tests/testthat/metadata-remove/metadata-1956b8-POST.R
@@ -1,5 +1,5 @@
 structure(list(method = "POST", url = "/metadata", status_code = 200L, 
-    headers = structure(list(Server = "nginx/1.23.1", Date = "Mon, 14 Nov 2022 22:50:24 GMT", 
+    headers = structure(list(Server = "nginx/1.23.1", Date = "Mon, 23 Jan 2023 15:38:58 GMT", 
         `Content-Length` = "0", Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
