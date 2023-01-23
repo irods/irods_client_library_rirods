532e6ee99c3ef2e12d7f078daedfb367b76f747f
trel <trel@users.noreply.github.com>
2023-01-23 18:45:45 +0000

    Snapshots http response

--- tests/testthat/add-data-collections/logicalpath-e27992-DELETE.R
+++ tests/testthat/add-data-collections/logicalpath-e27992-DELETE.R
@@ -1,6 +1,6 @@
 structure(list(method = "DELETE", url = "/logicalpath?logical-path=%2FtempZone%2Fhome%2Frods%2Ftestthat%2Fx%2Fa&no-trash=0&recursive=1", 
     status_code = 200L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Mon, 23 Jan 2023 15:38:49 GMT", `Content-Length` = "0", 
+        Date = "Mon, 23 Jan 2023 18:45:31 GMT", `Content-Length` = "0", 
         Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
--- tests/testthat/data-objects/stream-8d8e20-268c7e-PUT.R
+++ tests/testthat/data-objects/stream-8d8e20-268c7e-PUT.R
@@ -1,6 +1,6 @@
 structure(list(method = "PUT", url = "/stream?logical-path=%2FtempZone%2Fhome%2Frods%2Ftestthat%2FAirPassengers.rds&offset=0&count=3000&truncate=true", 
     status_code = 200L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Mon, 23 Jan 2023 15:38:51 GMT", `Content-Length` = "33", 
+        Date = "Mon, 23 Jan 2023 18:45:33 GMT", `Content-Length` = "33", 
         Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = as.raw(c(0x7b, 0x22, 0x63, 0x6f, 0x64, 0x65, 0x22, 
