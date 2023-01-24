c6459d443a3bd0c6a4a0226266bc6d5ec7c03d41
MartinSchobben <MartinSchobben@users.noreply.github.com>
2023-01-24 12:48:19 +0000

    Snapshots http response

--- tests/testthat/data-objects/stream-d7196b.R
+++ tests/testthat/data-objects/stream-d7196b.R
@@ -1,6 +1,6 @@
 structure(list(method = "GET", url = "/stream?logical-path=%2FtempZone%2Fhome%2Frods%2Ftestthat%2FAirPassengers.rds&offset=0&count=3000", 
     status_code = 200L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Mon, 23 Jan 2023 18:45:33 GMT", `Content-Length` = "427", 
+        Date = "Tue, 24 Jan 2023 12:48:06 GMT", `Content-Length` = "427", 
         Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = as.raw(c(0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 
--- tests/testthat/object-helpers/list-20ba52.R
+++ tests/testthat/object-helpers/list-20ba52.R
@@ -1,6 +1,6 @@
 structure(list(method = "GET", url = "/list?logical-path=%2FtempZone%2Fhome%2Ffrank&stat=0&permissions=0&metadata=0&offset=0&limit=100", 
     status_code = 400L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Mon, 23 Jan 2023 18:45:39 GMT", `Content-Length` = "95", 
+        Date = "Tue, 24 Jan 2023 12:48:12 GMT", `Content-Length` = "95", 
         Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
