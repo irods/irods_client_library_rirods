7cc0fffb501ea5588dfcd2fb293aabd3ef574847
MartinSchobben <schobbenmartin@gmail.com>
2023-06-08 21:50:50 +0200

    Tests: Add irods demo helper function and better http error handling

--- tests/testthat/add-data-collections/logicalpath-d7e80e-POST.R
+++ tests/testthat/add-data-collections/logicalpath-d7e80e-POST.R
@@ -1,6 +1,6 @@
 structure(list(method = "POST", url = "/logicalpath?logical-path=%2FtempZone%2Fhome%2Frods%2Ftestthat%2Fa&collection=1&create-parent-collections=0", 
     status_code = 200L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Mon, 05 Jun 2023 02:46:09 GMT", `Content-Length` = "0", 
+        Date = "Thu, 08 Jun 2023 19:45:32 GMT", `Content-Length` = "0", 
         Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
--- tests/testthat/data-objects/logicalpath-cffe2c-DELETE.R
+++ tests/testthat/data-objects/logicalpath-cffe2c-DELETE.R
@@ -1,6 +1,6 @@
 structure(list(method = "DELETE", url = "/logicalpath?logical-path=%2FtempZone%2Fhome%2Frods%2Ftestthat%2Fdfr.rds&no-trash=1&recursive=0", 
     status_code = 200L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Mon, 05 Jun 2023 02:46:12 GMT", `Content-Length` = "0", 
+        Date = "Thu, 08 Jun 2023 19:45:35 GMT", `Content-Length` = "0", 
         Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
