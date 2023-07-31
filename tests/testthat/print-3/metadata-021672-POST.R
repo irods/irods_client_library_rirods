02e6752869fa5f45bd7bd52b7f217bfc4a07a73e
trel <trel@users.noreply.github.com>
2023-07-31 02:34:18 +0000

    Snapshots http response

--- tests/testthat/print-3/metadata-021672-POST.R
+++ tests/testthat/print-3/metadata-021672-POST.R
@@ -1,5 +1,5 @@
 structure(list(method = "POST", url = "/metadata", status_code = 200L, 
-    headers = structure(list(Server = "nginx/1.23.1", Date = "Mon, 24 Jul 2023 02:35:04 GMT", 
+    headers = structure(list(Server = "nginx/1.23.1", Date = "Mon, 31 Jul 2023 02:34:08 GMT", 
         `Content-Length` = "0", Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
