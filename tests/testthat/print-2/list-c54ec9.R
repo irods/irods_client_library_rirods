ca04bed578caaa62f59f4381ba1ac5f4560070db
trel <trel@users.noreply.github.com>
2023-11-13 02:27:45 +0000

    Snapshots http response

--- tests/testthat/print-2/list-c54ec9.R
+++ tests/testthat/print-2/list-c54ec9.R
@@ -1,6 +1,6 @@
 structure(list(method = "GET", url = "/list?logical-path=%2FtempZone%2Fhome%2Frods%2Ftestthat%2Fqux.rds&stat=0&permissions=0&metadata=0&offset=0&limit=100", 
     status_code = 400L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Mon, 06 Nov 2023 02:27:30 GMT", `Content-Length` = "111", 
+        Date = "Mon, 13 Nov 2023 02:27:18 GMT", `Content-Length` = "111", 
         Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
