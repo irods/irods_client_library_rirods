569534f769a42f1cb7a5ea7822e5b42a6bc18a75
MartinSchobben <schobbenmartin@gmail.com>
2023-11-16 07:39:34 +0100

    tests hide date snapshots

--- tests/testthat/ils/list-f9b983.R
+++ tests/testthat/ils/list-f9b983.R
@@ -1,6 +1,6 @@
 structure(list(method = "GET", url = "/list?logical-path=%2Fprojectx&stat=0&permissions=0&metadata=0&offset=0&limit=100", 
     status_code = 400L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Mon, 20 Nov 2023 06:07:09 GMT", `Content-Length` = "84", 
-        Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
-        `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
+        Date = "", `Content-Length` = "84", Connection = "keep-alive", 
+        `Access-Control-Allow-Origin` = "*", `Access-Control-Allow-Headers` = "*", 
+        `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
