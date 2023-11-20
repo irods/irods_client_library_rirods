a6d64082ddd24e058d27ff611196cb7d618bc93f
trel <trel@users.noreply.github.com>
2023-11-20 02:31:11 +0000

    Snapshots http response

--- tests/testthat/metadata-remove/metadata-1956b8-POST.R
+++ tests/testthat/metadata-remove/metadata-1956b8-POST.R
@@ -1,5 +1,5 @@
 structure(list(method = "POST", url = "/metadata", status_code = 200L, 
-    headers = structure(list(Server = "nginx/1.23.1", Date = "Mon, 13 Nov 2023 02:27:23 GMT", 
+    headers = structure(list(Server = "nginx/1.23.1", Date = "Mon, 20 Nov 2023 02:30:49 GMT", 
         `Content-Length` = "0", Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = raw(0)), class = "httr2_response")
--- tests/testthat/small-data-objects/stream-8457e5.R
+++ tests/testthat/small-data-objects/stream-8457e5.R
@@ -1,6 +1,6 @@
 structure(list(method = "GET", url = "/stream?logical-path=%2FtempZone%2Fhome%2Frods%2Ftestthat%2Fdfr.csv&offset=0&count=2000", 
     status_code = 200L, headers = structure(list(Server = "nginx/1.23.1", 
-        Date = "Mon, 13 Nov 2023 02:27:14 GMT", `Content-Length` = "24", 
+        Date = "Mon, 20 Nov 2023 02:30:39 GMT", `Content-Length` = "24", 
         Connection = "keep-alive", `Access-Control-Allow-Origin` = "*", 
         `Access-Control-Allow-Headers` = "*", `Access-Control-Allow-Methods` = "AUTHORIZATION,ACCEPT,GET,POST,OPTIONS,PUT,DELETE"), class = "httr2_headers"), 
     body = as.raw(c(0x61, 0x2c, 0x62, 0x2c, 0x63, 0x0a, 0x61, 
